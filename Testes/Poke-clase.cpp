48005ea0c05b9ac839c70fba0138a751183a4452
Klapaucius <136605289+Pinheiro712@users.noreply.github.com>
2025-06-24 02:35:11 -0300

    first commit

---
+++ Testes/Poke-clase.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm> 
+using namespace std;
+
+
+vector<string> Pokedex = {
+        "Bulbasaur", "Charmander", "Squirtle", "Pidgey", "Rattata", "Pikachu", "Sandshrew",
+        "Nidoran", "Vulpix", "Jigglypuff", "Zubat", "Oddish", "Paras", "Venonat", "Diglett",
+        "Meowth", "Psyduck", "Machop", "Magnemite", "Gastly", "Onix", "Drowzee", "Krabby",
+        "Voltorb", "Exeggcute", "Cubone", "Koffing", "Rhyhorn", "Horsea", "Staryu"
+    };
+
+
+
+
+/*se eu for usar stats incluindo pokemons da 3 gem em diante tem que ser special attack */
+struct stats{
+    int hp;
+    int defesa;
+    int ataque;
+    int especialataq;
+    int especialdefesa;
+
+};
+
+
+class Pokemon{
+private:
+    string nome;
+    stats stats;    
+
+public:
+    Pokemon(string n, int hp, int atk, int def, int espa, int espd){
+        nome = n; 
+        stats.hp = hp;
+        stats.ataque = atk;
+        stats.defesa = def;
+        stats.especialataq = espa;
+        stats.especialdefesa = espd;
+    }
+
+    void mostrarpkm(){
+        cout<<"Pokemon: " << nome << endl;
+        cout<<"hp: " << stats.hp << endl;
+        cout<<"Ataque: " << stats.ataque << endl;
+        cout<<"Defesa: " << stats.defesa << endl;
+        cout<<"Ataque Especial: " << stats.especialataq << endl;
+        cout<<"Defesa Especial: " << stats.especialdefesa << endl;
+
+
+    };
+
+     void tomarDano(int dano) {
+        stats.hp -= dano;
+        if (stats.hp < 0) stats.hp = 0;
+     };
+
+};
+
+bool existe(const vector<string>& pokedex, string nome){
+        transform(nome.begin(), nome.end(), nome.begin(), ::tolower);
+
+        for(const auto& p : pokedex){
+            string  nomePokedex = p;
+            transform(nomePokedex.begin(), nomePokedex.end(), nomePokedex.begin(), ::tolower);
+
+            if (nomePokedex == nome){
+                return true;
+            }
+        }
+        return false;
+     }
+
+     
+
+int main(){
+    string nomeUsuario;
+    cout << "Digite o nome do pokemon: ";
+    cin >> nomeUsuario;
+
+    if (existe(Pokedex, nomeUsuario)){
+        cout << "Pokemon encontrado: "<< nomeUsuario << endl;
+    
+    }
+    else{
+        cout << "pokemon : " << nomeUsuario << "nao foi encontrado, tente novamente. "<< endl;
+    }
+    return 0;
+}
---
+++ Tipagem.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm> 
+#include <unordered_map>
+using namespace std;
+
+/* função que vai definir os tipos de cada pokemon e que vai puxar a tabela de tipos do jogo*/
+/*bora fazer os negocio de poo, ja que tamo sem oq fazer*/
+
+class Typeadv{
+private:
+    vector<string> tipos;
+    vector<vector<double>> matriz;
+    unordered_map<string, int> indice;
+
+
+public:
+    Typeadv(){
+         tipos = {"Normal", "Fire", "Water", "Electric", "Grass", "Ice", "Fight", "Poison", "Ground", "Flying", "Psychic", "Bug", "Rock", "Ghost", "Dragon"};
+    }
+    vector<vector<double>> matriz_ef = {
+    {1,1,1,1,1,1,1,1,1,1,1,0.5,0,1},/*normal*/ 
+    {1, 0.5, 0.5, 1, 2, 2, 1, 1, 1, 1, 1, 2, 0.5, 1, 0.5},/*fogo*/ 
+    {1,2,0.5,1,0.5,1,1,1,2,1,1,1,2,1,0.5},/*agua*/ 
+    {1,1,2,0.5,0.5,1,1,1,0,2,1,1,1,1,0.5},/*eletrico*/ 
+    {1,0.5,2,1,0.5,1,1,0.5,2,0.5,1,0.5,2,1,0.5},/*grama*/ 
+    {1,0.5,0.5,1,2,0.5,1,1,2,2,1,1,1,1,2},/*gelo*/ 
+    {2,1,1,1,1,2,1,0.5,1,0.5,0.5,2,0,1}, /*luta*/ 
+    {1,1,1,1,2,1,1,0.5,0.5,1,1,1,0.5,0.5,1},/*Veneno*/
+    {1,2,1,2,0.5,1,1,2,1,0,1,0.5,2,1,1},/*ground*/
+    {1,1,1,0.5,2,1,2,1,1,1,1,2,0.5,1,1},/*voador*/
+    {1,1,1,1,1,1,2,2,1,1,0.5,1,1,1,1},/*Psychic*/
+    {1,0.5,1,1,2,1,0.5,0.5,1,0.5,2,1,1,0.5,1},/*Bug*/
+    {1,2,1,1,1,2,0.5,1,0.5,2,1,2,1,1,1},/*rock*/
+    {0,1,1,1,1,1,1,1,1,1,2,1,1,2,1},/*ghost*/
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,2},/*Dragon*/
+    };
+    for (int i = 0 ; i < tipos.size(); i++){
+        indice[tipos[i]] = i;
+        }
+    };
+
+};
