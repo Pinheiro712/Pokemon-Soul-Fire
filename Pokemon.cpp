962098a2c996a90d4b35ff0601c0e4b07aab79d6
Klapaucius <jonhsudano@gmail.com>
2025-07-29 20:50:47 -0300

    vão substituir Poke-classe.cpp

---
+++ Pokemon.cpp
@@ -0,0 +1,25 @@
+#include "Pokemon.H"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+Pokemon::Pokemon(string n, int hp, int atk, int def, int espa, int espd, int spd) {
+    nome = n; 
+    stats.hp = hp;
+    stats.ataque = atk;
+    stats.defesa = def;
+    stats.especialataq = espa;
+    stats.especialdefesa = espd;
+    stats.speed = spd;
+}
+
+void Mostrarpkm (){
+    cout << "Pokemon: " << nome << endl;
+    cout << "hp: " << stats.hp << endl;
+    cout << "Ataque: " << stats.ataque << endl;
+    cout << "Defesa: " << stats.defesa << endl;
+    cout << "Ataque Especial: " << stats.especialataq << endl;
+    cout << "Defesa Especial: " << stats.especialdefesa << endl;
+    cout << "Velocidade: " << stats.speed << endl;
+};
